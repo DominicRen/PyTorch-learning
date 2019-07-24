89024633f2739d9d043ce8ae719d8eb02e359259
DominicRen <ren_9510@163.com>
2019-07-24 11:24:39 +0800

    VAE

--- 10 - AE and VAE/main.py
+++ 10 - AE and VAE/main.py
@@ -4,6 +4,7 @@ from torch import nn, optim
 from torchvision import transforms, datasets
 import visdom
 from ae import AE
+from vae import VAE
 
 
 
@@ -23,7 +24,7 @@ def  main():
     print('x:', x.shape)
 
     device = torch.device('cuda')
-    model = AE().to(device)
+    model = VAE().to(device)
     criteon = nn.MSELoss()
     optimizer = optim.Adam(model.parameters(), lr=1e-3)
     print(model)
@@ -35,19 +36,24 @@ def  main():
         for batchidx, (x, _) in enumerate(mnist_train):
             # [b, 1, 28, 28]
             x = x.to(device)
-            x_hat = model(x)
+            x_hat, kld = model(x)
             loss = criteon(x_hat, x)
+
+            if kld is not None:
+                elbo = - loss - 1.0 * kld
+                loss = - elbo
+
             # backprop
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
 
-        print(epoch, 'loss:', loss.item())
+        print(epoch, 'loss:', loss.item(), 'kld:', kld.item())
 
         x, _ = iter(mnist_test).next()
         x = x.to(device)
         with torch.no_grad():
-            x_hat = model(x)
+            x_hat, kld = model(x)
         viz.images(x, nrow=8, win='x', opts=dict(title='x'))
         viz.images(x_hat, nrow=8, win='x_hat', opts=dict(title='x_hat'))
 
